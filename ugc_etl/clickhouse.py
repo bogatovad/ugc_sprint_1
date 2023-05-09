85d918e52017746712bcc8dd852bf5e3da04d984
Seniacat <senya.sidorovich@gmail.com>
2023-05-09 11:14:49 +0300

    kafka-clickhouse integration

--- ugc_etl/clickhouse.py
+++ ugc_etl/clickhouse.py
@@ -6,9 +6,13 @@ from config import settings
 client = Client(host=settings.clickhouse_host)
 
 
-def create_tables(client):
+def init_clickhouse_db(client):
     client.execute(
-        """CREATE TABLE IF NOT EXISTS movie_views ON CLUSTER company_cluster(
+        "CREATE DATABASE IF NOT EXISTS cinema_analytics ON CLUSTER company_cluster",
+    )
+
+    client.execute(
+        """CREATE TABLE IF NOT EXISTS cinema_analytics.movie_views ON CLUSTER company_cluster(
         id Int64,
         user_id String,
         film_id String,
@@ -19,5 +23,30 @@ def create_tables(client):
     )
 
 
-def insert_to_clickhouse(client, values: list):
-    client.execute('INSERT INTO movie_views(...) VALUES (values)')
+def create_kafka_integration(client):
+    client.execute(
+        """CREATE TABLE IF NOT EXISTS cinema_analytics.kafka_movie_views(
+            id Int64,
+            user_id String,
+            film_id String,
+            movie_timestamp Int64
+            )
+            ENGINE = Kafka 
+            SETTINGS
+            kafka_broker_list = 'broker:29092',
+            kafka_topic_list = 'film_views',
+            kafka_group_name = 'clickhouse_reader',
+            kafka_format = 'JSONEachRow',
+            kafka_num_consumers = 1;"""
+    )
+
+
+def create_mv(client):
+    client.execute(
+        """CREATE MATERIALIZED VIEW IF NOT EXISTS cinema_analytics.mv_kafka_movie_views TO cinema_analytics.movie_views AS
+            SELECT * FROM cinema_analytics.kafka_movie_views;"""
+    )
+
+
+# def insert_to_clickhouse(client, values: list):
+#    client.execute('INSERT INTO movie_views(...) VALUES (values)')
--- ugc_etl/kafka_to_clickhouse.py
+++ ugc_etl/kafka_to_clickhouse.py
@@ -1,6 +1,20 @@
 from clickhouse_driver import Client
-from kafka import KafkaConsumer
+#from kafka import KafkaConsumer
+
+
+from clickhouse import init_clickhouse_db, create_kafka_integration, create_mv
+from config import settings
+
+
+client = Client(host=settings.clickhouse_host)
 
 
 def etl_process():
-    pass
+    init_clickhouse_db(client=client)
+    create_kafka_integration(client=client)
+    create_mv(client=client)
+    
+
+
+if __name__ == '__main__':
+    etl_process()
