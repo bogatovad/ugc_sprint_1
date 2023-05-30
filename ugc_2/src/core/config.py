7a909fc2dde5f368a9b83e26287ba6c387261246
Seniacat <senya.sidorovich@gmail.com>
2023-05-30 23:10:32 +0300

    fixed port in config

--- ugc_2/src/core/config.py
+++ ugc_2/src/core/config.py
@@ -9,7 +9,7 @@ logging_config.dictConfig(LOGGING)
 class Settings(BaseSettings):
     project_name: str = Field("ugc", env="PROJECT_NAME")
     host: str = Field("ugc_api_2", env="UGC_2_HOST")
-    port: int = Field(8080, env="UGC_2_PORT")
+    port: int = Field(8090, env="UGC_2_PORT")
     mongo_host: str = Field("mongodb", env="MONGO_HOST")
     mongo_port: int = Field(27017, env="MONGO_PORT")
     mongo_dbname: str = Field("ugc_movies", env="MONGO_DBNAME")
